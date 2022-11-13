5a06c056e351e44fa81c98aa8cde214bd8765867
Shakti Kc <shakti3462@gmail.com>
2022-11-13 16:06:40 +0545

    sorting issue fixed

--- src/ListOfRepository/components/useRepositoriesSorting.js
+++ src/ListOfRepository/components/useRepositoriesSorting.js
@@ -1,11 +1,18 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { SET_API_PARAMS } from "../../actionTypes/githubDataActionTypes";
 import useFetchRepoData from "../../api/useFetchRepoData";
+import { getApiParam } from "../../reducers/githubDataReducer";
 
 const useRepositoriesSorting = () => {
   const dispatch = useDispatch();
   const { fetchRepoData } = useFetchRepoData();
+
+  const apiParam = useSelector(getApiParam);
+
+  useEffect(() => {
+    fetchRepoData();
+  }, [apiParam.sort]);
 
   function sortingHandler(e) {
     dispatch({ type: SET_API_PARAMS, payload: { sort: e.target.value } });
--- src/SearchRepository/useSearchRepository.js
+++ src/SearchRepository/useSearchRepository.js
@@ -4,17 +4,13 @@ import useFetchRepoData from "../api/useFetchRepoData";
 
 import { SET_API_PARAMS } from "../actionTypes/githubDataActionTypes";
 import { getApiParam } from "../reducers/githubDataReducer";
-import { useEffect } from "react";
 
 const useSearchRepository = () => {
   const dispatch = useDispatch();
   const { fetchRepoData } = useFetchRepoData();
   const navigate = useNavigate();
-  const apiParam = useSelector(getApiParam);
 
-  useEffect(() => {
-    fetchRepoData();
-  }, [apiParam.sort]);
+  // useEffect(() => {}, []);
 
   function handelEnterEvent(event) {
     if (event.key === "Enter") {
@@ -22,7 +18,7 @@ const useSearchRepository = () => {
         type: SET_API_PARAMS,
         payload: { searchText: event.target.value },
       });
-
+      fetchRepoData();
       navigate("/repository/list");
       console.log("do validate", event.target.value);
     }
