bbc625d5e930517b31be73e00d2368b1bb632512
Shakti Kc <shakti3462@gmail.com>
2022-11-13 10:43:28 +0545

    react router intergration

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import SearchPageUI from "./ListOfRepository/ListOfRepository";
 import { Container } from "@chakra-ui/react";
 import SearchRepository from "./SearchRepository/SearchRepository";
 import DetailsOfRepository from "./DetailsOfRepository/DetailsOfRepository";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <Container maxW={"90vw"} bg="gray.50" w="100vw" h="100vh">
-      {" "}
-      {/* <SearchPageUI /> */}
-      <SearchRepository />
-      {/* <DetailsOfRepository /> */}
+      <BrowserRouter>
+        {" "}
+        <Routes>
+          <Route path="/" element={<SearchRepository />} />
+        </Routes>
+        {/* <SearchPageUI /> */}
+        {/* <DetailsOfRepository /> */}
+      </BrowserRouter>
     </Container>
   );
 }
--- src/SearchRepository/useSearchRepository.js
+++ src/SearchRepository/useSearchRepository.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import useFetchRepoData from "../api/useFetchRepoData";
 
 const useSearchRepository = () => {
   const { fetchRepoData } = useFetchRepoData();
+  const navigate = useNavigate();
   function handelEnterEvent(event) {
     if (event.key === "Enter") {
       fetchRepoData(event.target.value);
+      navigate("/repository/list");
       console.log("do validate", event.target.value);
     }
   }
